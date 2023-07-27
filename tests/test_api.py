682ff5ccdfd8d327ead69b92d5d437535fae3166
Atomie CHEN <atomic_cwh@163.com>
2023-07-27 11:12:46 +0800

    Improve test scripts

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,11 +3,14 @@ from handyllm import OpenAIAPI
 from dotenv import load_dotenv, find_dotenv
 # load env parameters from file named .env
 # API key is read from environment variable OPENAI_API_KEY
+# organization is read from environment variable OPENAI_ORGANIZATION
 load_dotenv(find_dotenv())
 
 ## or you can set these parameters in code
-OpenAIAPI.api_key = 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# OpenAIAPI.api_key = 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 # OpenAIAPI.organization = None
+
+# ----- EXAMPLE 1 -----
 
 prompt = [{
     "role": "user",
@@ -24,3 +27,19 @@ response = OpenAIAPI.chat(
     timeout=10
     )
 print(response['choices'][0]['message']['content'])
+
+
+print()
+print("-----")
+
+
+# ----- EXAMPLE 2 -----
+
+response = OpenAIAPI.completions(
+    model="text-davinci-002",
+    prompt="count to 23 and stop: 1,2,3,",
+    timeout=10,
+    max_tokens=256,
+    echo=True,  # Echo back the prompt in addition to the completion
+)
+print(response['choices'][0]['text'])
--- tests/test_stream.py
+++ tests/test_stream.py
@@ -3,8 +3,12 @@ from handyllm import OpenAIAPI
 from dotenv import load_dotenv, find_dotenv
 # load env parameters from file named .env
 # API key is read from environment variable OPENAI_API_KEY
+# organization is read from environment variable OPENAI_ORGANIZATION
 load_dotenv(find_dotenv())
 
+## or you can set these parameters in code
+# OpenAIAPI.api_key = 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# OpenAIAPI.organization = None
 
 # ----- EXAMPLE 1 -----
 
