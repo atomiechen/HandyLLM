48b1b6b2471f94245ff0a44c972b225c24924195
Atomie CHEN <atomic_cwh@163.com>
2024-07-04 14:01:25 +0800

    feat(__init__): update imports from requestor

--- src/handyllm/__init__.py
+++ src/handyllm/__init__.py
@@ -1,5 +1,5 @@
 from .openai_client import OpenAIClient, ClientMode
-from .requestor import Requestor
+from .requestor import *
 from .openai_api import OpenAIAPI
 from .endpoint_manager import Endpoint, EndpointManager
 from .prompt_converter import PromptConverter
