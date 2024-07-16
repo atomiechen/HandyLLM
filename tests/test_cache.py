0cccfd508a751cb7bf79de92477c824122c60a7c
Atomie CHEN <atomic_cwh@163.com>
2024-07-16 17:32:03 +0800

    test: fix test of async function

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -30,14 +30,14 @@ def test_cache_manager(tmp_path: Path, capsys: CaptureFixture[str]):
 @pytest.mark.asyncio
 async def test_cache_manager_async(tmp_path: Path, capsys: CaptureFixture[str]):
     cm = CacheManager(base_dir=tmp_path, enabled=True, save_only=False)
-    out = await cm.cache(func=async_func, out="test.txt")()
-    assert (tmp_path / "test.txt").read_text() == "hello"
+    out = await cm.cache(func=async_func, out="test_async.txt")()
+    assert (tmp_path / "test_async.txt").read_text() == "hello"
     assert out == "hello"
     # check print: func() should be called
     captured = capsys.readouterr()
     assert captured.out == "In async_func\n"
 
-    out2 = cm.cache(func=func, out="test.txt")()
+    out2 = await cm.cache(func=async_func, out="test_async.txt")()
     assert out2 == "hello"
     # check print: func() should not be called again
     captured = capsys.readouterr()
