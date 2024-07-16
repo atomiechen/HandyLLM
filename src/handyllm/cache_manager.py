ed8baa23100dbf1c8f9574d8114a818f1452832d
Atomie CHEN <atomic_cwh@163.com>
2024-07-16 17:02:52 +0800

    feat: add cache_manager

---
+++ src/handyllm/cache_manager.py
@@ -0,0 +1,101 @@
+from functools import wraps
+from inspect import iscoroutinefunction
+import json
+from os import PathLike
+from pathlib import Path
+from typing import Callable, Iterable, Optional, TypeVar, Union, cast
+from typing_extensions import ParamSpec
+import yaml
+
+from .types import PathType
+
+
+def _load_output_files(files: Iterable[Path]):
+    all_files_exist = all(Path(file).exists() for file in files)
+    if not all_files_exist:
+        return None
+    results = []
+    for file in files:
+        with open(file, 'r', encoding='utf-8') as f:
+            # determine the format according to the file suffix
+            if file.suffix.endswith('.yaml') or file.suffix.endswith('.yml'):
+                content = yaml.safe_load(f)
+            elif file.suffix.endswith('.json'):
+                content = json.load(f)
+            else:
+                content = f.read()
+        results.append(content)
+    if len(results) == 1:
+        return results[0]
+    return tuple(results)
+
+def _save_output_files(files: list[Path], results):
+    if not isinstance(results, tuple):
+        results = (results,)
+    if len(files) != len(results):
+        raise ValueError('The number of files and results should be the same.')
+    for file, result in zip(files, results):
+        file.parent.mkdir(parents=True, exist_ok=True)
+        with open(file, 'w', encoding='utf-8') as f:
+            # determine the format according to the file suffix
+            if file.suffix.endswith('.yaml') or file.suffix.endswith('.yml'):
+                yaml.dump(result, f, default_flow_style=False, allow_unicode=True)
+            elif file.suffix.endswith('.json'):
+                json.dump(result, f, ensure_ascii=False, indent=2)
+            else:
+                f.write(result)
+
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+class CacheManager:
+    def __init__(self, base_dir: PathType, enabled: bool = True, save_only: bool = False):
+        self.base_dir = base_dir
+        self.enabled = enabled
+        self.save_only = save_only
+
+    def cache(
+        self, 
+        func: Callable[P, R], 
+        out: Union[PathType, Iterable[PathType]],
+        enabled: Optional[bool] = None,
+        save_only: Optional[bool] = None,
+    ) -> Callable[P, R]:
+        '''
+        Store the output of the function to the specified file. 
+        The number of files must be the same as that of the results. 
+        Supported file formats: yaml, json, txt
+        '''
+        if enabled is None:
+            enabled = self.enabled
+        if save_only is None:
+            save_only = self.save_only
+        if not enabled:
+            return func
+        if isinstance(out, str) or isinstance(out, PathLike):
+            out = [out]
+        full_files = [Path(self.base_dir, file) for file in out]
+        if iscoroutinefunction(func):
+            @wraps(func)
+            async def async_wrapped_func(*args: P.args, **kwargs: P.kwargs):
+                if not save_only:
+                    results = _load_output_files(full_files)
+                    if results is not None:
+                        return cast(R, results)
+                results = await func(*args, **kwargs)
+                _save_output_files(full_files, results)
+                return cast(R, results)
+            return cast(Callable[P, R], async_wrapped_func)
+        else:
+            @wraps(func)
+            def sync_wrapped_func(*args: P.args, **kwargs: P.kwargs):
+                if not save_only:
+                    results = _load_output_files(full_files)
+                    if results is not None:
+                        return cast(R, results)
+                results = func(*args, **kwargs)
+                _save_output_files(full_files, results)
+                return cast(R, results)
+            return cast(Callable[P, R], sync_wrapped_func)
+
