7d9901bc2b2520f658fd89c9dff46c76d25ea440
Atomie CHEN <atomic_cwh@163.com>
2024-07-26 03:03:52 +0800

    test: add test for hprompt chat fetch()

---
+++ tests/test_hprompt.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+import re
+from handyllm import ChatPrompt, load_from
+import responses
+
+
+tests_dir = Path(__file__).parent
+
+@responses.activate
+def test_chat_fetch():
+    mock_data = {
+        "id": "chatcmpl-123",
+        "object": "chat.completion",
+        "created": 1677652288,
+        "model": "gpt-4o-mini",
+        "system_fingerprint": "fp_44709d6fcb",
+        "choices": [{
+            "index": 0,
+            "message": {
+                "role": "assistant",
+                "content": "\n\nHello there, how may I assist you today?",
+            },
+            "logprobs": None,
+            "finish_reason": "stop"
+        }],
+        "usage": {
+            "prompt_tokens": 9,
+            "completion_tokens": 12,
+            "total_tokens": 21
+        }
+    }
+    
+    responses.add(
+        method=responses.POST,
+        url=re.compile(r'.*'),
+        json=mock_data,
+    )
+    
+    prompt_file = tests_dir / 'assets' / 'chat.hprompt'
+    prompt = load_from(prompt_file, cls=ChatPrompt)
+    response = prompt.fetch(api_key='fake-key')
+    assert response.choices[0].message.role == "assistant"
+
