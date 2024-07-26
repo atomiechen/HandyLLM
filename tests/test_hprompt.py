e70d3a51c8206cd74f1e1fe6e91a788056e87cc3
Atomie CHEN <atomic_cwh@163.com>
2024-07-27 02:58:20 +0800

    test: add test for hprompt async chat fetch

--- tests/test_hprompt.py
+++ tests/test_hprompt.py
@@ -9,6 +9,28 @@ import respx
 
 tests_dir = Path(__file__).parent
 
+mock_fetch_data = {
+    "id": "chatcmpl-123",
+    "object": "chat.completion",
+    "created": 1677652288,
+    "model": "gpt-4o-mini",
+    "system_fingerprint": "fp_44709d6fcb",
+    "choices": [{
+        "index": 0,
+        "message": {
+            "role": "assistant",
+            "content": "\n\nHello there, how may I assist you today?",
+        },
+        "logprobs": None,
+        "finish_reason": "stop"
+    }],
+    "usage": {
+        "prompt_tokens": 9,
+        "completion_tokens": 12,
+        "total_tokens": 21
+    }
+}
+
 mock_stream_data = [
     {"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-4o-mini", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{"role":"assistant","content":""},"logprobs":None,"finish_reason":None}]},
     {"id":"chatcmpl-123","object":"chat.completion.chunk","created":1694268190,"model":"gpt-4o-mini", "system_fingerprint": "fp_44709d6fcb", "choices":[{"index":0,"delta":{"content":"Hello"},"logprobs":None,"finish_reason":None}]},
@@ -19,39 +41,26 @@ tmp = ["data: " + json.dumps(data) for data in mock_stream_data]
 tmp.append("data: [DONE]")
 body = "\n".join(tmp)
 
+
 @responses.activate
 def test_chat_fetch():
-    mock_data = {
-        "id": "chatcmpl-123",
-        "object": "chat.completion",
-        "created": 1677652288,
-        "model": "gpt-4o-mini",
-        "system_fingerprint": "fp_44709d6fcb",
-        "choices": [{
-            "index": 0,
-            "message": {
-                "role": "assistant",
-                "content": "\n\nHello there, how may I assist you today?",
-            },
-            "logprobs": None,
-            "finish_reason": "stop"
-        }],
-        "usage": {
-            "prompt_tokens": 9,
-            "completion_tokens": 12,
-            "total_tokens": 21
-        }
-    }
-    
     responses.add(
         method=responses.POST,
         url=re.compile(r'.*'),
-        json=mock_data,
+        json=mock_fetch_data,
     )
-    
     prompt_file = tests_dir / 'assets' / 'chat.hprompt'
     prompt = load_from(prompt_file, cls=ChatPrompt)
     response = prompt.fetch(api_key='fake-key')
+    assert response.choices[0].message["role"] == "assistant"
+
+@pytest.mark.asyncio
+@respx.mock
+async def test_async_chat_fetch():
+    respx.post(re.compile(r'.*')).respond(json=mock_fetch_data)
+    prompt_file = tests_dir / 'assets' / 'chat.hprompt'
+    prompt = load_from(prompt_file, cls=ChatPrompt)
+    response = await prompt.afetch(api_key='fake-key')
     assert response.choices[0].message["role"] == "assistant"
 
 @responses.activate
