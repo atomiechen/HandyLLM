4320dc2fd89d05a1b6504f1470418173fd9606fb
Atomie CHEN <atomic_cwh@163.com>
2024-07-27 23:37:38 +0800

    test: add test for CompletionsPrompt sync / async stream

--- tests/test_completions_prompt.py
+++ tests/test_completions_prompt.py
@@ -1,7 +1,8 @@
+import json
 from pathlib import Path
 import re
 
-from handyllm import CompletionsPrompt, load_from
+from handyllm import CompletionsPrompt, load_from, stream_completions, astream_completions
 import pytest
 import responses
 import respx
@@ -30,6 +31,15 @@ mock_fetch_data = {
   }
 }
 
+mock_stream_data = [
+    {"choices":[{"text":""}]},
+    {"choices":[{"text":"This"}]},
+    {"choices":[{"text":" is indeed a test"}]},
+]
+tmp = ["data: " + json.dumps(data) for data in mock_stream_data]
+tmp.append("data: [DONE]")
+stream_body = "\n".join(tmp)
+
 
 @responses.activate
 def test_completions_fetch():
@@ -49,12 +59,39 @@ async def test_async_completions_fetch():
     assert response.choices[0].text == "\n\nThis is indeed a test"
 
 @responses.activate
+def test_completions_stream():
+    responses.add(responses.POST, url=re.compile(r'.*'), body=stream_body)
+    prompt_file = tests_dir / 'assets' / 'completions.hprompt'
+    prompt = load_from(prompt_file, cls=CompletionsPrompt)
+    response = prompt.stream(api_key='fake-key')
+    content = ""
+    for text in stream_completions(response):
+        content += text
+    assert content == "This is indeed a test"
+
+@pytest.mark.asyncio
+@respx.mock
+async def test_async_completions_stream():
+    respx.post(re.compile(r'.*')).respond(text=stream_body)
+    prompt_file = tests_dir / 'assets' / 'completions.hprompt'
+    prompt = load_from(prompt_file, cls=CompletionsPrompt)
+    response = prompt.astream(api_key='fake-key')
+    content = ""
+    async for text in astream_completions(response):
+        content += text
+    assert content == "This is indeed a test"
+
+@responses.activate
 def test_completions_run():
     responses.add(responses.POST, url=re.compile(r'.*'), json=mock_fetch_data)
     prompt_file = tests_dir / 'assets' / 'completions.hprompt'
     prompt = load_from(prompt_file, cls=CompletionsPrompt)
     result_prompt = prompt.run(api_key='fake-key')
     assert result_prompt.result_str == "\n\nThis is indeed a test"
+    
+    responses.replace(responses.POST, url=re.compile(r'.*'), body=stream_body)
+    result_prompt = prompt.run(api_key='fake-key', stream=True)
+    assert result_prompt.result_str == "This is indeed a test"
 
 @pytest.mark.asyncio
 @respx.mock
@@ -64,4 +101,8 @@ async def test_async_completions_run():
     prompt = load_from(prompt_file, cls=CompletionsPrompt)
     result_prompt = await prompt.arun(api_key='fake-key')
     assert result_prompt.result_str == "\n\nThis is indeed a test"
+    
+    respx.post(re.compile(r'.*')).respond(text=stream_body)
+    result_prompt = await prompt.arun(api_key='fake-key', stream=True)
+    assert result_prompt.result_str == "This is indeed a test"
 
